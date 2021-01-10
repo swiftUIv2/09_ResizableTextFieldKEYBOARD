aa3ee6b4f8a948f483ec7043ab24ca2bcb57bb8f
emm <723125@gmail.com>
2021-01-10 13:36:18 +0000

    Initial Commit

---
+++ ResizableTextFieldKEYBOARD_09/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ResizableTextFieldKEYBOARD_09
+//
+//  Created by emm on 10/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ResizableTextFieldKEYBOARD_09/ResizableTextFieldKEYBOARD_09App.swift
@@ -0,0 +1,17 @@
+//
+//  ResizableTextFieldKEYBOARD_09App.swift
+//  ResizableTextFieldKEYBOARD_09
+//
+//  Created by emm on 10/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct ResizableTextFieldKEYBOARD_09App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
